1700d02bf4d69d1116fe9c3e1ca77c53049a2083
Pablo Figueroa <pablofigue03@gmail.com>
2022-09-08 11:56:23 -0300

    Fix varios

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ Cuotas:
 */
 
 
-function calcularMonto() {
+/* function calcularMonto() {
     let compraFinal = Math.random() * 100
     return compraFinal
-}
+} */
 
 function calcularEfectivo(monto) {
     let montoFinal
@@ -39,38 +39,6 @@ function calcularCuota(monto, cuotasNum) {
     return [finalCuota.toFixed(2), montoCuota.toFixed(2)]
 }
 
-alert("Requisitos: ser mayor de 18" + "\n" 
-+ "Efectivo: Solo hay descuento del 10% en compras mayores a uSd50" + "\n" 
-+ "Cuotas: 3 con 3% recarga - 6 con 5% recarga - 9 con 8% de recarga")
-
-
-let edad = prompt("Ingrese su edad: ")
-
-if (edad < 18) {
-    alert("Debe ser mayor de 18 para realizar una compra")
-} else {
-    let medio = prompt("Seleccione el medio de pago (efectivo o cuotas): ")
-    while (medio != "efectivo" && medio != "cuotas") {
-        alert("Medio de pago erroneo")
-        medio = prompt("Seleccione nuevamente entre efectivo o cuotas")
-    }
-    
-    let montoSimulado = calcularMonto()
-    if (medio == "efectivo") {
-        montoEfvo = calcularEfectivo(montoSimulado)
-        alert("El monto a pagar es: " + montoEfvo)
-    } else {
-        let cuotasNum = prompt("Ingrese cantidad de cuotas (3, 6 o 9) :" )
-        while (cuotasNum != 3 && cuotasNum != 6 && cuotasNum != 9) {
-            alert("cantidad de cuotas incorrectas")
-            cuotasNum = prompt("Ingrese la cantidad de cuotas nuevamente")
-        }
-        [finalCuota, montoCuota] = calcularCuota(montoSimulado, cuotasNum)
-        alert("El monto a pagar es: " + finalCuota + "\n"
-        + "y cada cuota es de: " + montoCuota)
-    }
-}
-
 class Juego {
     constructor(nombre, consola, precio) {
         this.nombre = nombre.toLowerCase()
@@ -78,22 +46,58 @@ class Juego {
         this.precio = parseFloat(precio)
     }
 }
-
-let juegoCompra = []
-let cantidad = prompt("Ingrese cantidad de juegos a comprar")
-controlador = 1
-do {
-    controlador += 1
-    let nombre = prompt("Ingrese nombre del juego: ")
-    let consola = prompt("Ingrese la consola")
-    let precio = prompt("Ingrese el precio")
-    const juegoAdquirido = new Juego(nombre, consola, precio)
-
-    juegoCompra.push(juegoAdquirido)
-} while (controlador <= cantidad)
-
-for (i=0; i < juegoCompra.length; i++) {
-    console.log(juegoCompra[i].nombre, juegoCompra[i].consola, juegoCompra[i].precio)
-}
     
+alert("Requisitos: ser mayor de 18" + "\n" 
++ "Efectivo: Solo hay descuento del 10% en compras mayores a uSd50" + "\n" 
++ "Cuotas: 3 con 3% recarga - 6 con 5% recarga - 9 con 8% de recarga")
+
+let edad = prompt("Ingrese su edad: ")
+
+if (edad < 18) {
+    alert("Debe ser mayor de 18 para realizar una compra")
+} else {
+    /* Generamos array con length = cantidad de juegos a comprar */
+    let juegoCompra = []
+    let cantidad = prompt("Ingrese cantidad de juegos a comprar")
+    controlador = 1
+    do {
+        controlador += 1
+        let nombre = prompt("Ingrese nombre del juego") /* Unico valor libre para validar salida de consola */
+        let consola = "PC" /* Lo pasamos predefinido en PC para el simulacro */
+        let precio = Math.random() * 50 /* Precio lo pasamos como un valor aleatorio a los fines de simulacro*/
+        precio = precio.toFixed(2)
+        const juegoAdquirido = new Juego(nombre, consola, precio)
+
+        juegoCompra.push(juegoAdquirido) /* Cargamos las compras al array correspondiente*/
+    } while (controlador <= cantidad)
+
+    /* Generamos una variable para acumular el precio total de la compra */
+    precioCargado = 0
+    /* Iteramos sobre el array para calcular el precio total y para imprimir la compra en consola */
+    for (i=0; i < juegoCompra.length; i++) {
+        precioCargado += juegoCompra[i].precio
+        console.log("juego:", juegoCompra[i].nombre, "consola:", juegoCompra[i].consola, "precio:", juegoCompra[i].precio)
+    }
+
+    let medio = prompt("Seleccione el medio de pago (efectivo o cuotas): ")
+    while (medio != "efectivo" && medio != "cuotas") {
+        alert("Medio de pago erroneo")
+        medio = prompt("Seleccione nuevamente entre efectivo o cuotas")
+    }
+    
+    let montoSimulado = precioCargado
+    if (medio == "efectivo") {
+        montoEfvo = calcularEfectivo(montoSimulado)
+        console.log("El monto a pagar es: " + montoEfvo)
+    } else {
+        let cuotasNum = prompt("Ingrese cantidad de cuotas (3, 6 o 9) :" )
+        while (cuotasNum != 3 && cuotasNum != 6 && cuotasNum != 9) {
+            alert("cantidad de cuotas incorrectas")
+            cuotasNum = prompt("Ingrese la cantidad de cuotas nuevamente")
+        }
+        [finalCuota, montoCuota] = calcularCuota(montoSimulado, cuotasNum)
+        console.log("El monto a pagar es: " + finalCuota + "\n"
+        + "y cada cuota es de: " + montoCuota)
+    }
+}
 
